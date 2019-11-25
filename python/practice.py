3f92f66297363922edbc1bda7e55f5645527190c
narcissus <2935549510@qq.com>
2019-11-25 23:55:54 +0800

    just for daily practie

--- python/practice.py
+++ python/practice.py
@@ -1,4 +1,18 @@
 #!/usr/bin/python
 
-import tkinter as tk
+import os
 
+
+dir = '/home/narcissus/Downloads/test'
+os.chdir(dir)
+file = open('test.txt',mode='a+',encoding='UTF-8')
+file.write('wodld\n')
+file.write('hello\n')
+file.write('hehe\n')
+#print(os.listdir())
+#help(os)
+#list = os.listdir()
+#print(list)
+#file = open('tags',mode='r',encoding='utf-8')
+#for i in file:
+#    print(i)
